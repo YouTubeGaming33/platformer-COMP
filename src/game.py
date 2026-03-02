8cf5b9d54ce263028b6dc9f9a95653b7629bb980
j-x-smith <jamiexuerebsmith100@gmail.com>
2026-03-02 13:14:37 +0000

    Level handler + tiles setup

--- src/game.py
+++ src/game.py
@@ -5,6 +5,8 @@ and the game is created.
 """
 import pygame
 from settings import SCREEN_HEIGHT, SCREEN_WIDTH, FRAMERATE
+from src.components.tiles import TestTile
+from src.components.levelhandler import parse_level, load_level
 
 class Game:
     """
@@ -31,6 +33,11 @@ class Game:
         self.delta_time = 0.1
         self.running = True
         self.state = "PLAYING"
+        
+        self.render_layer = pygame.sprite.Group()
+        self.update_layer = pygame.sprite.Group()
+
+        self.load_new_level(1)
 
     def tick(self):
         """
@@ -41,7 +48,13 @@ class Game:
         """
         self.delta_time = self.clock.tick() / 1000
         self.delta_time = min(0.1, max(self.delta_time, 0.001))
-    
+
+    def load_new_level(self, level_num):
+        self.render_layer.empty()
+        self.update_layer.empty()
+        level_data = parse_level(f"assets/levels/level{level_num}.txt")
+        self.level = load_level(level_data, self)
+
     def main(self):
         """
         The main function is where the main game loop is.
@@ -51,6 +64,8 @@ class Game:
         while self.running:
             self.tick()
             
+            self.update_layer.update()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -58,6 +73,7 @@ class Game:
             match self.state:
                 case "PLAYING":
                     self.screen.fill((255,255,255))
+                    self.render_layer.draw(self.screen)
 
         
             pygame.display.flip()
